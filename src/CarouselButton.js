5c568368a846344aad75d5db9d09003f483ac18a
greendream <michaelt3r4@gmail.com>
2023-05-25 23:00:14 -0400

    :sparkles: Initial implementation of CarouselButton

--- src/CarouselButton.js
+++ src/CarouselButton.js
@@ -1,5 +1,12 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const CarouselButton = () => <button />;
+//const CarouselButton = ({children}) => <button>{children}</button>;
+const CarouselButton = props => <button {...props} />;
+
+CarouselButton.propTypes = {
+    children: PropTypes.node.isRequired,
+};
 
 export default CarouselButton;
--- src/tests/CarouselButton.test.js
+++ src/tests/CarouselButton.test.js
@@ -1,13 +1,38 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import {configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+// eslint-disable-next-line no-unused-vars
 import CarouselButton from '../CarouselButton';
 
 configure({ adapter: new Adapter() });
 
 describe( 'CarouselButton', () => {
+  const text = 'Button text';
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<CarouselButton>{text}</CarouselButton>);
+  });
+
   it( 'renders a <button>', () => {
-    const wrapper = shallow(<CarouselButton />);
+    // const wrapper = shallow(<CarouselButton />);
     expect(wrapper.type()).toBe('button');
   });
+
+  it("passes 'children' through to the <button>", () => {
+    expect(wrapper.prop('children')).toBe(text);
+
+  });
+
+  it('passes other props through to the <button>', () => {
+    const onClick = () => {};
+    const className = 'my-carousel-button';
+    const dataAction = 'prev';
+    wrapper.setProps({ onClick, className, 'data-action': dataAction });
+    expect(wrapper.prop('onClick')).toBe(onClick);
+    expect(wrapper.prop('className')).toBe(className);
+    expect(wrapper.prop('data-action')).toBe(dataAction);
+  });
+
 });
