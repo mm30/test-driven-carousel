33dd1c892fe43db33481c96af507898a295e6227
greendream <michaelt3r4@gmail.com>
2023-06-13 16:44:45 -0400

    halfway to Add example page

---
+++ example/slides.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+const referralParams = 'utm_source=test-driven-carousel&utm_medium=referral';
+
+// eslint-disable-next-line no-unused-vars
+const getUsernameUrl = username => `https://unsplash.com/@${username}?${referralParams}`;
+
